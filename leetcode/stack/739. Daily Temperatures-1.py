bb6b3957bd75944efb39436bb86144c0d24e21a6
microcephalus7 <microcephalus7@gmail.com>
2021-11-16 20:01:47 +0900

    Add 739

---
+++ leetcode/stack/739. Daily Temperatures-1.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0] * len(temperatures)
+        stack = []
+
+        for index, value in enumerate(temperatures):
+
+            while stack and value > temperatures[stack[-1]]:
+                last = stack.pop()
+                answer[last] = index - last
+
+            stack.append(index)
+
+        return answer
