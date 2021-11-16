cefbcdedb85a6d38852662de49687c1472a0b858
microcephalus7 <microcephalus7@gmail.com>
2021-11-16 19:50:38 +0900

    Add 316

---
+++ leetcode/stack/316. Remove Duplicate Letters-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeDuplicateLetters(self, s):
+
+        for c in sorted(set(s)):
+            suffix = s[s.index(c):]
+
+            if set(suffix) == set(s):
+                return c + self.removeDuplicateLetters(suffix.replace(c, ''))
+
+        return ''
