811c9b41f0df961307ad3efa19c0644bbfcceaf4
microcephalus7 <microcephalus7@gmail.com>
2021-11-16 20:15:49 +0900

    Add 225

---
+++ leetcode/queue/225. Implement Stack using Queues-1.py
@@ -0,0 +1,22 @@
+from typing import Deque
+
+
+class MyStack:
+
+    def __init__(self):
+        self.queue: Deque = collections.deque()
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+        for _ in range(len(self.queue) - 1):
+            self.queue.append(self.queue.popleft())
+
+    def pop(self) -> int:
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        return len(self.queue) == 0
