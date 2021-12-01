e554c501881b3897bbb12985d2df331b2dfc40b3
microcephalus7 <microcephalus7@gmail.com>
2021-12-01 17:07:33 +0900

    Add 24

---
+++ leetcode/linked list/24. Swap Nodes in Pairs-3.py
@@ -0,0 +1,13 @@
+# 재귀 구조
+
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head and head.next:
+
+            p = head.next
+            # 스왑된 값 리턴 받음
+            head.next = self.swapPairs(p.next)
+            p.next = head
+            return p
+
+        return head
