f201013f2a3a2c3bf41516b8cfa75245589c33fe
microcephalus7 <microcephalus7@gmail.com>
2021-12-02 20:50:13 +0900

    Add 46

---
+++ leetcode/graph/46. Permutations-1.py
@@ -0,0 +1,26 @@
+# dfs
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if not nums:
+            return []
+
+        result = []
+        prev_elements = []
+
+        def dfs(elements):
+            if len(elements) == 0:
+                result.append(prev_elements[:])
+
+            for i in elements:
+                next_elements = elements[:]
+                next_elements.remove(i)
+
+                prev_elements.append(i)
+                dfs(next_elements)
+
+                prev_elements.pop()
+
+        dfs(nums)
+
+        return result
---
+++ leetcode/graph/46. Permutations-2.py
@@ -0,0 +1,5 @@
+# itertools
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        return list(itertools.permutations(nums))
