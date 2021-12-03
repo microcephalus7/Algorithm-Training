fa9a66892366f8d7ef5d99f7750c2c1d15cd62e1
microcephalus7 <microcephalus7@gmail.com>
2021-12-03 21:33:16 +0900

    Add 1261

---
+++ leetcode/tree/1261. Find Elements in a Contaminated Binary Tree-2.py
@@ -0,0 +1,31 @@
+# DFS, recur, in
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class FindElements:
+
+    def __init__(self, root: Optional[TreeNode]):
+
+        self.values = []
+
+        def dfs(node, val):
+            self.values.append(val)
+            val = 2 * val
+            if node.left:
+                dfs(node.left, val + 1)
+            if node.right:
+                dfs(node.right, val + 2)
+
+        dfs(root, 0)
+
+    def find(self, target: int) -> bool:
+        return target in self.values
+
+
+# Your FindElements object will be instantiated and called as such:
+# obj = FindElements(root)
+# param_1 = obj.find(target)
