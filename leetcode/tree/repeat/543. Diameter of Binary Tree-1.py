01a6897d4614bf4f99981b10075e8ea6b4a2e50c
microcephalus7 <microcephalus7@gmail.com>
2021-12-03 17:55:14 +0900

    Add 543

---
+++ leetcode/tree/repeat/543. Diameter of Binary Tree-1.py
@@ -0,0 +1,29 @@
+# dfs, recur, max
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    longest: int = 0
+
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+
+        def dfs(node: TreeNode) -> int:
+            if not node:
+                return 0
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            self.longest = max(self.longest, left + right)
+
+            return max(left, right) + 1
+
+        dfs(root)
+
+        return self.longest
