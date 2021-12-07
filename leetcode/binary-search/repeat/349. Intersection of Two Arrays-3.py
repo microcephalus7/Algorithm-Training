e53591f6f16e4b5421c4765df215594936ed38f8
microcephalus7 <microcephalus7@gmail.com>
2021-12-07 20:29:11 +0900

    Add 349

---
+++ leetcode/binary-search/repeat/349. Intersection of Two Arrays-3.py
@@ -0,0 +1,15 @@
+# set, 이진검색, bisect
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+        result = set()
+        nums2.sort()
+
+        for n1 in nums1:
+            i2 = bisect.bisect_left(nums2, n1)
+
+            if len(nums2) > 0 and len(nums2) > i2 and n1 == nums2[i2]:
+                result.add(n1)
+
+        return result
