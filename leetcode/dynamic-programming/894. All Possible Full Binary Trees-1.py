70c9a8f1a7900a91f4376bad3920f0d04baf0c62
microcephalus7 <microcephalus7@gmail.com>
2021-12-09 19:07:59 +0900

    Add 894

---
+++ leetcode/dynamic-programming/894. All Possible Full Binary Trees-1.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
+        result = []
+
+        def dfs(node, count):
+            if count == n:
+                result.append()
+
+        dfs()
+
+        return result
