7bc9a910cf511262c56ce1397a27bc0ef2791e2e
microcephalus7 <microcephalus7@gmail.com>
2021-12-14 14:03:51 +0900

    Add 1721

---
+++ leetcode/linked list/1721. Swapping Nodes in a Linked List-1.py
@@ -0,0 +1,26 @@
+# k 번째 노드, Len - k 번쨰 노드 스왑
+# 값만 바꾸기
+# 반복
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+
+    def swapNodes(self, head: ListNode, k: int) -> ListNode:
+        first = last = head
+
+        for i in range(k - 1):
+            first = first.next
+
+        null_checker = first
+
+        while null_checker.next:
+            last = last.next
+            null_checker = null_checker.next
+
+        first.val, last.val = last.val, first.val
+
+        return head
