8cffc300ce7081c8e7605dcdab944d916332ce4b
microcephalus7 <microcephalus7@gmail.com>
2021-12-20 13:09:31 +0900

    Add 316 comment

--- leetcode/stack/316. Remove Duplicate Letters-1.py
+++ leetcode/stack/316. Remove Duplicate Letters-1.py
@@ -1,6 +1,9 @@
+# 문자열에서 중복 문자 제거 후 사전식 순서로 나열
+# 재귀
 class Solution:
     def removeDuplicateLetters(self, s):
 
+        # 집합으로 정렬
         for c in sorted(set(s)):
             suffix = s[s.index(c):]
 
