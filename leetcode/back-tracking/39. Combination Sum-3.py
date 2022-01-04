5a068a7b370e48b5c117b2f1199f332678988d3a
microcephalus7 <microcephalus7@gmail.com>
2022-01-04 13:52:02 +0900

    Add 39

---
+++ leetcode/back-tracking/39. Combination Sum-3.py
@@ -0,0 +1,27 @@
+# 중복이 허용되는 조합의 합
+# DFS, 백 트래킹
+# 백트래킹 코드 참고
+# 최적화
+
+class Solution(object):
+    def combinationSum(self, candidates, target):
+
+        def dfs(nums, target, path):
+            if target < 0:
+                return
+            if target == 0:
+                ret.append(path)
+                return
+            for i in range(len(nums)):
+
+                # 점프 코드
+                # 최적화
+                if nums[i] > target:
+                    break
+
+                dfs(nums[i:], target-nums[i], path+[nums[i]])
+
+        ret = []
+        # 선 정렬
+        dfs(sorted(candidates), target, [])
+        return ret
