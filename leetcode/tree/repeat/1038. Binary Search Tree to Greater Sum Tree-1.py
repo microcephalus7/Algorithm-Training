698a9bcda0d3680c80c3db6d2543e7de8d6b74ab
microcephalus7 <microcephalus7@gmail.com>
2022-02-11 16:32:49 +0900

    Add 1038

--- leetcode/tree/repeat/1038. Binary Search Tree to Greater Sum Tree-1.py
+++ leetcode/tree/repeat/1038. Binary Search Tree to Greater Sum Tree-1.py
@@ -1,21 +1,26 @@
 # BST 를 GST 로 변환하기
 # DFS,
 # 중위 순위 탐색
+# 참고
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     val: int = 0
 
     def bstToGst(self, root: TreeNode) -> TreeNode:
         if root:
-            self.bstToGst(root.right)
+            # 오른쪽
+            root.right = self.bstToGst(root.right)
+            # 중간
             self.val += root.val
             root.val = self.val
-            self.bstToGst(root.left)
-
+            # 왼쪽
+            root.left = self.bstToGst(root.left)
         return root
