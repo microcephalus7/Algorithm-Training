89a99a5ac7f0340cdc4d784378deee4ea37e7d5e
microcephalus7 <microcephalus7@gmail.com>
2022-02-16 14:05:37 +0900

    Add 414

--- leetcode/heap/414. Third Maximum Number-2.py
+++ leetcode/heap/414. Third Maximum Number-2.py
@@ -1,12 +1,12 @@
 # 배열에서 중복을 거른 후 세번째로 큰 수
-# list
-# set
+# heapq
+# nlargest
 
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
         nums = sorted(list(set(nums)))
 
         if len(nums) > 2:
-            return nums[len(nums) - 3]
+            return heapq.nlargest(3, nums)[-1]
         else:
-            return nums[-1]
+            return heapq.nlargest(1, nums)[0]
